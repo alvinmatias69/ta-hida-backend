6be384b0067516a1c1eae5d2e2edc9701a5b61d7
Matias Alvin <alvinmatias69@gmail.com>
2018-01-07 20:36:20 +0700

    Reworked RadacctController Change RadacctController works. It doesn't use Radacct model's anymore, instead it uses generic DB model. Data is get from joined of Radacct and lokasi_mac table.

--- app/Http/Controllers/RadacctController.php
+++ app/Http/Controllers/RadacctController.php
@@ -2,19 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use App\Radacct;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class RadacctController extends Controller {
   public function get () {
-    //$radacct = Radacct::all();
-    $radacct = Radacct::select('username', 'calledstationid', 'callingstationid', 'acctstarttime')->get();
+    $radacct = DB::table('radacct')
+                  ->leftJoin('lokasi_mac', 'radacct.calledstationid', '=', 'lokasi_mac.calledstationid')
+                  ->select('username', 'lokasi', 'callingstationid', 'acctstarttime')
+                  ->get();
 
     return response()->json($radacct);
   }
 
   public function find (Request $request) {
-    $radacct = Radacct::select('username', 'calledstationid', 'callingstationid', 'acctstarttime')->where('username', 'like', '%' . $request->input('username') . '%')->get();
+    $radacct = DB::table('radacct')
+                  ->leftJoin('lokasi_mac', 'radacct.calledstationid', '=', 'lokasi_mac.calledstationid')
+                  ->select('username', 'lokasi', 'callingstationid', 'acctstarttime')
+                  ->where('username', 'like', '%' . $request->input('username') . '%')
+                  ->get();
 
     return response()->json($radacct);
   }
